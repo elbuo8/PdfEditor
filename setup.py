aad16bbbf981ee62f28d50be14dc2107f654d31c
elbuo8 <yamil.asusta@upr.edu>
2013-05-06 02:47:30 -0400

    Added git repo

--- setup.py
+++ setup.py
@@ -3,9 +3,10 @@
 from distutils.core import setup
 
 setup(name='PdfEditor',
-      version='0.0.1',
+      version='0.0.2',
       description='Modify previously created PDF\'s',
       author='Yamil Asusta (elbuo8)',
       author_email='yamil.asusta@upr.edu',
       license='MIT',
+      url='https://github.com/elbuo8/PdfEditor',
       )
