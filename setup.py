8770d676fa62c5f4bd29fc226a249c0452b67ed6
elbuo8 <yamil.asusta@upr.edu>
2013-05-06 13:28:41 -0400

    Better dependency management

--- setup.py
+++ setup.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
 
-setup(name='PdfEditor',
-      version='0.0.2',
-      description='Modify previously created PDF\'s',
-      author='Yamil Asusta (elbuo8)',
-      author_email='yamil.asusta@upr.edu',
-      license='MIT',
-      url='https://github.com/elbuo8/PdfEditor',
-      )
+setup(
+    name='PdfEditor',
+    version='0.0.3',
+    description='Modify previously created PDF\'s',
+    author='Yamil Asusta (elbuo8)',
+    author_email='yamil.asusta@upr.edu',
+    license='MIT',
+    url='https://github.com/elbuo8/PdfEditor',
+    install_requires=[
+        'reportlab'
+    ],
+    dependency_links=[
+        'http://github.com/knowah/PyPDF2/tarball/master'
+    ],
+)
